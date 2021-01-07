cbbe98231572588b37652a7075e0d2c94d48ef9b
huayans <huayansantiago@gmail.com>
2021-01-07 11:07:52 -0300

    home add

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,11 @@ export class AppComponent implements OnInit {
   public selectedIndex = 0;
   public appPages = [
     {
+      title: 'Home',
+      url: '/folder/Home',
+      icon: 'home'
+    },
+    {
       title: 'Inbox',
       url: '/folder/Inbox',
       icon: 'mail'
